b3db7c8834f5c946340e933ff2135fd46266e0d8
Ahmet <69368572+N1mblexe@users.noreply.github.com>
2025-08-08 12:20:11 +0300

    optimization fixes
    Pre calculates: origin offset and spacing size

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -3,28 +3,40 @@ using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
 {
+    [Header("CELL")]
     [SerializeField] private GameObject cellPrefab;
+
     [SerializeField] private List<List<GameObject>> currentCells = new List<List<GameObject>>();
+
     [SerializeField] private Vector2 cellSize = Vector2.one;
-    [SerializeField] private Vector2Int size;
-    [SerializeField] private float spacing = .1f;
-    private static readonly Vector2Int[] sizeLimites = new Vector2Int[]
+
+    [Header("SIZE&POSITIONING")]
+    [SerializeField]
+    private Vector2Int[] sizeBoundaries = new Vector2Int[2]
     {
         new Vector2Int(2, 2),
         new Vector2Int(16 , 9)
     };
+    [SerializeField] private Vector2Int size;
+
+    [SerializeField] private float spacing = .1f;
+    private Vector2 originOffset;
+    private Vector2 spacedCellSize;
+
     public void GenerateGrids()
     {
-        if (!CheckSizeLimits(size))
+        /*if (currentCells == null)
+        {
+            Debug.LogWarning("No instance of currentGrids");
+            return;
+        }*/
+
+        if (!CheckSizeLimits())
         {
             Debug.LogError($"Invalid size : {size}");
             return;
         }
-        //if (currentCells == null)
-        //{
-        //    Debug.LogWarning("No instance of currentGrids");
-        //    return;
-        //}
+
         if (cellPrefab == null)
         {
             Debug.LogError("cellPrefab is null");
@@ -32,13 +44,14 @@ public class GridGenerator : MonoBehaviour
         }
 
         ClearCurrentCells();
+        PreCalculate();
 
         for (uint x = 0; x < size.x; x++)
         {
             List<GameObject> row = new List<GameObject>();
             for (uint y = 0; y < size.y; y++)
             {
-                var cellPositon = CalculateCellPos(size, new Vector2(x, y));
+                var cellPositon = CalculateCellPos(new Vector2(x, y));
                 if (cellPositon == Vector2.negativeInfinity)
                 {
                     Debug.LogError($"Invalid cell position relative to the size : ({x} , {y})");
@@ -57,11 +70,12 @@ public class GridGenerator : MonoBehaviour
 
     public void ClearCurrentCells()
     {
-        //if (currentCells == null)
-        //{
-        //    Debug.LogWarning("no instance of currentGrids");
-        //    return;
-        //}
+        /*if (currentCells == null)
+        {
+            Debug.LogWarning("no instance of currentGrids");
+            return;
+        }*/
+
         //Delete cells
         foreach (var list in currentCells)
         {
@@ -82,18 +96,10 @@ public class GridGenerator : MonoBehaviour
     /// <param name="size"></param>
     /// <param name="celllPos">position in the array(not the transform position)</param>
     /// <returns></returns>
-    private Vector2 CalculateCellPos(Vector2Int size, Vector2 cellPos)
+    private Vector2 CalculateCellPos(Vector2 cellPos)
     {
         if ((size.x <= cellPos.x || size.y <= cellPos.y) || (cellPos.x < 0 || cellPos.y < 0))
             return Vector2.negativeInfinity;
-
-        Vector2 spacedCellSize = new Vector2(cellSize.x + spacing, cellSize.y + spacing);
-
-        Vector2 originOffset = new Vector2
-        (
-            ((size.x - 1) * spacedCellSize.x) / 2f,
-            ((size.y - 1) * spacedCellSize.y) / 2f
-        );
 
         Vector2 position = new Vector2
         (
@@ -104,12 +110,31 @@ public class GridGenerator : MonoBehaviour
         return position;
     }
 
-    private bool CheckSizeLimits(Vector2 size)
+    private void PreCalculate()
     {
-        return size.x >= sizeLimites[0].x &&
-               size.y >= sizeLimites[0].y &&
-               size.x <= sizeLimites[1].x &&
-               size.y <= sizeLimites[1].y;
+        spacedCellSize = CalculateSpacedCellSize();
+        originOffset = CalculateOriginOffset();
+    }
+
+    private Vector2 CalculateSpacedCellSize()
+    {
+        return new Vector2(cellSize.x + spacing, cellSize.y + spacing);
+    }
+    private Vector2 CalculateOriginOffset()
+    {
+        return new Vector2
+        (
+            ((size.x - 1) * spacedCellSize.x) / 2f,
+            ((size.y - 1) * spacedCellSize.y) / 2f
+        );
+    }
+
+    private bool CheckSizeLimits()
+    {
+        return size.x >= sizeBoundaries[0].x &&
+               size.y >= sizeBoundaries[0].y &&
+               size.x <= sizeBoundaries[1].x &&
+               size.y <= sizeBoundaries[1].y;
     }
 
     #endregion
